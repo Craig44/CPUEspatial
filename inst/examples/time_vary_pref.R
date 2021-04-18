4e4b08d0fdcd04271e68d05068f0794de23fe130
Craig44 <craig.marsh@bigbluedata.co.nz>
2021-04-19 11:48:23 +1200

    ADD: r script looking at time-varying preference.

---
+++ inst/examples/time_vary_pref.R
@@ -0,0 +1,11 @@
+#'
+#' Time-vary pref with a non-timevarying GF
+#' An issue I for see is conflict between these two data sources, are sample locaitons changed because
+#' of preferential sampling or the underlying density?
+#' 
+
+
+
+
+
+
